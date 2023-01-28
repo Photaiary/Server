2ca0d055473245c501d7c247795b73ea89f033f5
ParkJoungWoo <jug78@naver.com>
2023-01-29 03:05:53 +0900

    초기 설정

---
+++ Photaiary/src/main/java/com/photaiary/Photaiary/PhotaiaryApplication.java
@@ -0,0 +1,13 @@
+package com.photaiary.Photaiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotaiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotaiaryApplication.class, args);
+	}
+
+}
