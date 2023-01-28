85183f5460eab77cb91991be1ea85b8ffb8e3f97
ParkJoungWoo <jug78@naver.com>
2023-01-29 03:05:53 +0900

    초기 설정

---
+++ Photaiary/src/test/java/com/photaiary/Photaiary/PhotaiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photaiary.Photaiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotaiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
