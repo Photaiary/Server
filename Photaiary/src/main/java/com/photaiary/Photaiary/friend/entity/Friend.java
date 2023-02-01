fa1a80097e8033da64b7367c34b5a4a0fa48d0ba
sunny <schang1008@gmail.com>
2023-02-01 21:42:49 +0900

    Feat:[ ]Add Friend Entity
    Friend 엔티티 추가, StatusType enum 추가, User 엔티티 임의 생성
    
    Resolves: none

---
+++ Photaiary/src/main/java/com/photaiary/Photaiary/friend/entity/Friend.java
@@ -0,0 +1,35 @@
+package com.photaiary.Photaiary.friend.entity;
+
+import com.photaiary.Photaiary.user.entity.User;
+import com.sun.istack.NotNull;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Friend{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @NotNull
+    private int Index;
+
+
+    @JoinColumn(name="userIndex")
+    @NotNull
+    private User userIndex;
+
+    @JoinColumn(name="userIndex")
+    @NotNull
+    private User friendIndex;
+
+    @ColumnDefault("active")
+    @Enumerated(EnumType.STRING)
+    private StatusType status;
+
+}
