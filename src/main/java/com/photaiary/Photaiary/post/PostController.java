5eb98e34ca081d8d920e065bab3011426b5a2a6e
LimJuHyeok <ovg07047@naver.com>
2023-02-08 19:56:39 +0900

    Feat: Add controller for updating posts API

--- src/main/java/com/photaiary/Photaiary/post/PostController.java
+++ src/main/java/com/photaiary/Photaiary/post/PostController.java
@@ -6,7 +6,6 @@ import com.photaiary.Photaiary.post.diary.DiaryPostRequestDto;
 import com.photaiary.Photaiary.post.diary.DiaryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -20,13 +19,14 @@ public class PostController {
     private final DiaryService diaryService;
     private int status;
 
+    //Response Body
+
+
     @PostMapping("/daily/{dailyIndex}/diary")
     public Map<Integer, Object> postDiary(@PathVariable Long dailyIndex, @RequestBody DiaryPostRequestDto requestDto) {
-
-        Diary postedDiary = diaryService.save(requestDto);
-
         Map<Integer, Object> response = new HashMap<>();
         Map<String, Object> data = new HashMap<>();
+        Diary postedDiary = diaryService.save(requestDto);
 
         if(postedDiary.getDiaryIndex() == null){
             status = HttpStatus.BAD_REQUEST.value();
@@ -41,8 +41,30 @@ public class PostController {
         return response;
     }
 
-    @GetMapping("/daily/{dailyIndex}/diary")
-    public List<Diary> findAll(@PathVariable Long dailyIndex){
+    //추가
+    @PutMapping("/diary/{dailyIndex}")
+    public Map<Integer,Object> update(@PathVariable Long dailyIndex, @RequestBody DiaryPostRequestDto requestDto){
+        Diary updatedDiary = diaryService.updateByIdAndDiaryIndex(dailyIndex, requestDto);
+
+        Map<Integer, Object> response = new HashMap<>();
+        Map<String, Object> data = new HashMap<>();
+
+       if(updatedDiary == null){
+            //diaryTitle이나 diaryContent값이 null이면 변경 x
+           status = HttpStatus.BAD_REQUEST.value();
+           data.put("isSuccess", "false");
+           response.put(status, data);
+        }else if(updatedDiary != null){
+           status = HttpStatus.OK.value();
+           data.put("isSuccess", "true");
+           response.put(status, data);
+       }
+        return response;
+    }
+
+    //postman 테스트용 Diary 전체조회
+    @GetMapping("diary/test")
+    public List<Diary> findAll(){
         return diaryService.findAll();
     }
 }
