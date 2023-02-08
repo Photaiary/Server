94352dc0f634d748d5dba5e7dc1ab7ac768e1d2e
LimJuHyeok <ovg07047@naver.com>
2023-02-08 21:15:09 +0900

    Test: Add 게시글수정test

--- src/test/java/com/photaiary/Photaiary/PostControllerTest.java
+++ src/test/java/com/photaiary/Photaiary/PostControllerTest.java
@@ -1,6 +1,7 @@
 package com.photaiary.Photaiary;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.photaiary.Photaiary.post.daily.Daily;
 import com.photaiary.Photaiary.post.diary.Diary;
 import com.photaiary.Photaiary.post.diary.DiaryPostRequestDto;
@@ -14,12 +15,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @ExtendWith(SpringExtension.class)
@@ -41,17 +44,14 @@ public class PostControllerTest {
 
     @Test
     public void 게시글작성() throws Exception{
+        // ※ daily에 대한 테스트 내용 -> 차후, Daily와 User Token 만든 이후 테스트 코드에 추가
+
         //given
         DiaryPostRequestDto requestDto = DiaryPostRequestDto.builder()
                 .diaryTitle("title")
                 .diaryContent("content")
                 .build();
 
-//        Diary savedDiary = postsRepository.save(Diary.builder()
-//                .diaryTitle("title")
-//                .diaryContent("content")
-//                .daily(daily)
-//                .build());
         Diary savedDiary = postsRepository.save(requestDto.toEntity());
 
         Long savedId = savedDiary.getDiaryIndex();
@@ -68,6 +68,38 @@ public class PostControllerTest {
 
         List<Diary> diaries = postsRepository.findAll();
         assertThat(diaries.get(0).getDiaryIndex()).isEqualTo(savedId);
+        assertThat(diaries.get(0).getDiaryTitle()).isEqualTo(expectedTitle);
+        assertThat(diaries.get(0).getDiaryContent()).isEqualTo(expectedContent);
+    }
+
+    @Test
+    public void 게시글수정() throws Exception{
+        //given
+        Diary savedDiary = postsRepository.save(Diary.builder()
+                .diaryTitle("diaryTitle")
+                .diaryContent("diaryContent")
+                .build());
+
+        Long updateId = savedDiary.getDiaryIndex();
+        String expectedTitle = "Changed Title";
+        String expectedContent = "Changed Content";
+
+        DiaryPostRequestDto requestDto = DiaryPostRequestDto.builder()
+                .diaryTitle(expectedTitle)
+                .diaryContent(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/diary/1";
+
+        HttpEntity<DiaryPostRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        //when
+        ResponseEntity<Diary> responseEntity = testRestTemplate.exchange(url, HttpMethod.PUT, requestEntity, Diary.class);
+
+        //then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        List<Diary> diaries = postsRepository.findAll();
         assertThat(diaries.get(0).getDiaryTitle()).isEqualTo(expectedTitle);
         assertThat(diaries.get(0).getDiaryContent()).isEqualTo(expectedContent);
     }
