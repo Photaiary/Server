d3bdebb2e6f13b0af5e5c4b525d2ea1ec784fc1e
ParkJoungWoo <jug78@naver.com>
2023-02-10 16:42:38 +0900

    feat(daily/*) : add Daily Entity and Repository
    - Daily entity
    - Daily Repository

--- src/main/java/com/photaiary/Photaiary/post/daily/entity/Daily.java
+++ src/main/java/com/photaiary/Photaiary/post/daily/entity/Daily.java
@@ -1,4 +1,4 @@
-package com.photaiary.Photaiary.post.daily;
+package com.photaiary.Photaiary.post.daily.entity;
 
 import com.photaiary.Photaiary.user.entity.User;
 import lombok.Builder;
@@ -11,14 +11,13 @@ import javax.persistence.*;
 @NoArgsConstructor
 @Entity
 public class Daily {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "dailyIndex")
     private Long dailyIndex;
 
-    @ManyToOne
-    @JoinColumn(name = "userIndex")
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @JoinColumn(name = "userIndex", nullable = false)
     private User user;
 
     @Column(name = "dailyValue")
