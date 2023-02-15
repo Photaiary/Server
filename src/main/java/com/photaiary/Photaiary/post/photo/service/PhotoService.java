fc31a20dfacc9d3d849d088e460c8328efddc561
ParkJoungWoo <jug78@naver.com>
2023-02-15 18:19:43 +0900

    refactor(photo/service) : Add Exception to Service
    - add NoUserException to Service when Daily is not Vaild

--- src/main/java/com/photaiary/Photaiary/post/photo/service/PhotoService.java
+++ src/main/java/com/photaiary/Photaiary/post/photo/service/PhotoService.java
@@ -2,14 +2,22 @@ package com.photaiary.Photaiary.post.photo.service;
 
 import com.photaiary.Photaiary.post.daily.entity.Daily;
 import com.photaiary.Photaiary.post.daily.repository.DailyReposiotry;
+import com.photaiary.Photaiary.post.photo.controller.exception.custom.NoUserException;
 import com.photaiary.Photaiary.post.photo.dto.EditRequest;
 import com.photaiary.Photaiary.post.photo.dto.PhotoRequest;
+import com.photaiary.Photaiary.post.photo.dto.PhotoS3Dto;
+import com.photaiary.Photaiary.post.photo.entity.DeleteStatus;
 import com.photaiary.Photaiary.post.photo.entity.Photo;
 import com.photaiary.Photaiary.post.photo.repository.PhotoRepository;
+import com.photaiary.Photaiary.post.photo.validator.PhotoRequestValidator;
+import com.photaiary.Photaiary.post.photo.vo.PhotoVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -17,33 +25,28 @@ import java.util.Optional;
 public class PhotoService {
     private final PhotoRepository photoRepository;
     private final DailyReposiotry dailyReposiotry;
+    private final PhotoRequestValidator photoRequestValidator;
 
     @Transactional
-    public Long photoSave(PhotoRequest photoRequest) {
+    public Long photoInfoSave(PhotoRequest photoRequest, PhotoVo photoVo, PhotoS3Dto photoS3Dto) throws Exception {
         Optional<Daily> daily = dailyReposiotry.findById(photoRequest.getDailyId());
-        try {
-            if (daily.isPresent()) {
-                Photo photo = new Photo()
-                        .builder()
-                        .latitude(photoRequest.getLatitude())
-                        .longitude(photoRequest.getLongitude())
-                        .image(photoRequest.getImage())
-                        .comment(photoRequest.getComment())
-                        .deleteStatus(photoRequest.getDeleteStatus())
-                        .tag(photoRequest.getTag())
-                        .daily(daily.get())
-                        .build();
-                return photoRepository.save(photo).getId();
-            } else if (daily.isEmpty()) {
-                System.out.println("empty");
-                return -1000L;
-            } else {
-                return -900L;
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-            return -800L;
+        if (daily.isPresent()) {
+            Photo photo = new Photo().builder()
+                    .comment(photoRequest.getComment())
+                    .latitude(photoVo.getLatitude())
+                    .longitude(photoVo.getLongitude())
+                    .deleteStatus(DeleteStatus.exist)
+                    // image Location을 PhotoS3Dto에서 받아와야한다.
+                    .image("location")
+                    .daily(daily.get())
+                    .tag(photoRequestValidator.getStringTag(photoRequest.getTagListString()))
+                    .build();
+            return photoRepository.save(photo).getId();
+        } else if (daily.isEmpty()) {
+            throw new NoUserException("nono");
+//            return -700L;
         }
+        return -800L;
     }
 
     @Transactional
@@ -66,7 +69,20 @@ public class PhotoService {
         }
     }
 
-//    private Map<String, String> photoLocation() {
-//
+    @Transactional
+    public String getImage(Long uuid) {
+        Optional<Photo> photo = photoRepository.findById(uuid);
+        if (photo.isPresent()) {
+            return photo.get().getImage();
+        } else if (!photo.isPresent()) {
+            return "noPhoto";
+        }
+        return "error";
+    }
+
+//    @Transactional
+//    public List<String> getImageByDaily(Long userId, String dailyValue) {
+//        Daily today = dailyReposiotry.findByDailyValue  (dailyValue);
+//        Optional<Photo> photo = photoRepository.findBy()
 //    }
 }
