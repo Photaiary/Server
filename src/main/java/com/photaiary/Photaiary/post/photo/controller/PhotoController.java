38f10d29a8ecba291f0d766b41509dbe0c3eb988
ParkJoungWoo <jug78@naver.com>
2023-02-16 20:00:55 +0900

    fix(photo/controller) : fix Controller bug
    - fix controller bug

--- src/main/java/com/photaiary/Photaiary/post/photo/controller/PhotoController.java
+++ src/main/java/com/photaiary/Photaiary/post/photo/controller/PhotoController.java
@@ -1,5 +1,6 @@
 package com.photaiary.Photaiary.post.photo.controller;
 
+import com.photaiary.Photaiary.post.photo.dto.SinglePhotoDto;
 import com.photaiary.Photaiary.post.photo.dto.EditRequest;
 import com.photaiary.Photaiary.post.photo.dto.PhotoRequest;
 import com.photaiary.Photaiary.post.photo.dto.PhotoS3Dto;
@@ -9,31 +10,33 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/photo")
 public class PhotoController {
     private final PhotoService photoService;
-    
-//    @PostMapping("/upload")
-//    public Long upload(@RequestBody PhotoRequest photoRequest){
-//        return photoService.photoInfoSave(photoRequest);
-//    }
 
     @PutMapping("/edit")
     public Long update(@RequestBody EditRequest editRequest) {
         return photoService.photoEdit(editRequest);
     }
 
-    @PostMapping("/uploadtest")
-    public Long uploadTest(@RequestParam("img") MultipartFile img, @RequestPart PhotoRequest photoRequest) throws Exception {
+    @PostMapping("/upload")
+    public Long upload(@RequestParam("img") MultipartFile img, @RequestPart PhotoRequest photoRequest) throws Exception {
         PhotoVo photoVo = new PhotoVo(img);
         PhotoS3Dto photoS3Dto = new PhotoS3Dto();
         return photoService.photoInfoSave(photoRequest, photoVo, photoS3Dto);
     }
 
+    @DeleteMapping("/")
+    public Boolean delete(@RequestBody SinglePhotoDto singlePhotoDto) {
+        return photoService.photoDelete(singlePhotoDto);
+    }
+
+    @GetMapping("/")
+    public String viewOnePhoto(@RequestBody SinglePhotoDto singlePhotoDto) {
+        return photoService.viewSinglePhoto(singlePhotoDto);
+    }
     @GetMapping("/upload")
     public String yes() {
         return "yes";
