e9c613e8267330bb02102ec499d41676e32ea6a7
ParkJoungWoo <jug78@naver.com>
2023-02-20 16:43:03 +0900

    fix(main/) : add Cross Origin All(Back Door)

--- src/main/java/com/photaiary/Photaiary/PhotaiaryApplication.java
+++ src/main/java/com/photaiary/Photaiary/PhotaiaryApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableJpaAuditing
+@CrossOrigin("*")
 public class PhotaiaryApplication {
 
 	public static void main(String[] args) {
