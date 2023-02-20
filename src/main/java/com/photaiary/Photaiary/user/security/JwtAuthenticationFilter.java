13f97bcd83d1ed5fe2620e33dd718e47e4f1a7fe
sangminee <lsmlee99@gmail.com>
2023-02-21 03:36:33 +0900

    fix(main/security) : 인증오류 고침

--- src/main/java/com/photaiary/Photaiary/user/security/JwtAuthenticationFilter.java
+++ src/main/java/com/photaiary/Photaiary/user/security/JwtAuthenticationFilter.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
-//    private User user;
 
     public JwtAuthenticationFilter(JwtProvider jwtProvider) {
         this.jwtProvider = jwtProvider;
@@ -22,17 +21,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        HttpServletResponse res = (HttpServletResponse) response;
-        res.setHeader("Access-Control-Allow-Origin", "*"); //허용대상 도메인
-        res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT");
-//        response.setHeader("Access-Control-Max-Age", "3600");
-        res.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
-
-
         String token = jwtProvider.resolveToken(request);
-        String email=jwtProvider.getEmail(token);
-//        this.user=
-
 
         if (token != null && jwtProvider.validateToken(token)) {
             // check access token
--- src/main/java/com/photaiary/Photaiary/user/security/JwtProvider.java
+++ src/main/java/com/photaiary/Photaiary/user/security/JwtProvider.java
@@ -145,5 +145,4 @@ public class JwtProvider {
             return false;
         }
     }
-
 }
