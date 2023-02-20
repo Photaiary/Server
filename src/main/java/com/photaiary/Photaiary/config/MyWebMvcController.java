1c573633130a21101932375acf894af15ee266e5
ParkJoungWoo <jug78@naver.com>
2023-02-20 17:07:06 +0900

    fix(config/*) : fix CORS ERROR configuration file

--- src/main/java/com/photaiary/Photaiary/config/MyWebMvcController.java
+++ src/main/java/com/photaiary/Photaiary/config/MyWebMvcController.java
@@ -2,16 +2,17 @@ package com.photaiary.Photaiary.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class MyWebMvcController implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .maxAge(3600);
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
