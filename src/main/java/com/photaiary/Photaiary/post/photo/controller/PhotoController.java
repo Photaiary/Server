e899f37d7f08691ecac9c60f21da5945b245cf24
ParkJoungWoo <jug78@naver.com>
2023-02-22 01:41:13 +0900

    refactor(photo/controller) : change request, response

--- src/main/java/com/photaiary/Photaiary/post/photo/controller/PhotoController.java
+++ src/main/java/com/photaiary/Photaiary/post/photo/controller/PhotoController.java
@@ -1,8 +1,10 @@
 package com.photaiary.Photaiary.post.photo.controller;
 
+import com.photaiary.Photaiary.post.photo.dto.PhotoResponse;
 import com.photaiary.Photaiary.post.photo.dto.SinglePhotoDto;
 import com.photaiary.Photaiary.post.photo.dto.EditRequest;
 import com.photaiary.Photaiary.post.photo.dto.PhotoRequest;
+import com.photaiary.Photaiary.post.photo.entity.Photo;
 import com.photaiary.Photaiary.post.photo.service.PhotoService;
 import com.photaiary.Photaiary.post.photo.vo.BucketVo;
 import com.photaiary.Photaiary.post.photo.vo.PhotoVo;
@@ -21,11 +23,17 @@ public class PhotoController {
         return photoService.photoEdit(editRequest);
     }
 
+    @ResponseBody
     @PostMapping("/upload")
-    public Long upload(@RequestParam("img") MultipartFile img, @RequestPart PhotoRequest photoRequest) throws Exception {
+    public PhotoResponse upload(@RequestParam("img") MultipartFile img, @RequestPart PhotoRequest photoRequest) throws Exception {
         PhotoVo photoVo = new PhotoVo(img);
-        BucketVo bucketVo = new BucketVo(img);
-        return photoService.photoInfoSave(photoRequest, photoVo, bucketVo);
+        BucketVo bucketVo = new BucketVo(photoService.s3Upload(img));
+        PhotoResponse photoResponse = PhotoResponse.builder()
+                .photoIndex(photoService.photoInfoSave(photoRequest, photoVo, bucketVo))
+                .longitude(photoVo.getLongitude())
+                .latitude(photoVo.getLatitude())
+                .build();
+        return photoResponse;
     }
 
     @DeleteMapping("/")
