eceeb11f7fc3335f56a0e2acc0ff0dc8143f7233
LimJuHyeok <ovg07047@naver.com>
2023-02-21 20:16:58 +0900

    Feat: Add CustomException

--- src/main/java/com/photaiary/Photaiary/friend/exception/custom/CustomException.java
+++ src/main/java/com/photaiary/Photaiary/friend/exception/custom/CustomException.java
@@ -3,7 +3,7 @@ package com.photaiary.Photaiary.friend.exception.custom;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class CustomException extends Exception{
+public class CustomException extends Exception{//👨‍💻
     public CustomException(String message) {
         super(message);
     }
