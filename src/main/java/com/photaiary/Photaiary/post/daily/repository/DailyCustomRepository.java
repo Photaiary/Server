7c48e16e6b4999040af2f1bb3708afb28908a5ea
LimJuHyeok <ovg07047@naver.com>
2023-02-22 18:02:31 +0900

    Feat(daily/repository): Add DaolyCustomRepository

--- src/main/java/com/photaiary/Photaiary/post/daily/repository/DailyCustomRepository.java
+++ src/main/java/com/photaiary/Photaiary/post/daily/repository/DailyCustomRepository.java
@@ -1,2 +1,10 @@
-package com.photaiary.Photaiary.post.daily.repository;public interface DailyCustomRepository {
+package com.photaiary.Photaiary.post.daily.repository;
+
+import com.photaiary.Photaiary.post.daily.entity.Daily;
+import com.photaiary.Photaiary.user.entity.User;
+
+import java.util.Optional;
+
+public interface DailyCustomRepository {
+    Optional<Daily> getDailyByUserAndValue(User user, String dailyValue);
 }
