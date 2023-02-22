be23e4856f53ea7a3839bcdadfbc8909534b5f16
ParkJoungWoo <jug78@naver.com>
2023-02-22 15:10:38 +0900

    chore(global/handler) : change Response

--- src/main/java/com/photaiary/Photaiary/global/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/photaiary/Photaiary/global/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.photaiary.Photaiary.global.exception;
 
 import com.photaiary.Photaiary.post.photo.controller.exception.PhotoErrorHandler;
+import com.photaiary.Photaiary.post.photo.controller.exception.custom.PhotoExceptionResponse;
 import com.photaiary.Photaiary.post.photo.controller.exception.custom.VoException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,7 +19,7 @@ import java.util.Date;
 @ControllerAdvice // 모든 Controller 가 실행될 때 반드시 실행됨
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-//    @ExceptionHandler(Exception.class)
+    //    @ExceptionHandler(Exception.class)
 //    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
 //        ExceptionResponse exceptionResponse =
 //                new ExceptionResponse(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
@@ -29,27 +30,27 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
     @ExceptionHandler(VoException.class)
     public final ResponseEntity<Object> handlePhotoExceptions(Exception ex, WebRequest request) {
         PhotoErrorHandler photoErrorHandler = new PhotoErrorHandler(ex, request);
-        ExceptionResponse exceptionResponse = photoErrorHandler.handleError();
+        PhotoExceptionResponse exceptionResponse = photoErrorHandler.handleError();
         return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR); // 500 error
     }
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
-        ExceptionResponse exceptionResponse =
-                new ExceptionResponse(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                        ex.getMessage(), request.getDescription(false));
-
-        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR); // 500 error
-    }
+    //    @ExceptionHandler(Exception.class)
+//    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
+//        ExceptionResponse exceptionResponse =
+//                new ExceptionResponse(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value(),
+//                        ex.getMessage(), request.getDescription(false));
+//
+//        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR); // 500 error
+//    }
     @Override  // 부모가 가진 method를 재정의
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers,
                                                                   HttpStatus status,
                                                                   WebRequest request) {
-        ExceptionResponse exceptionResponse =
+        ExceptionResponse photoExceptionResponse =
                 new ExceptionResponse(new Date(), HttpStatus.BAD_REQUEST.value(),
                         "Validation Failed", ex.getBindingResult().toString());
 
-        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity(photoExceptionResponse, HttpStatus.BAD_REQUEST);
     }
 
 }
