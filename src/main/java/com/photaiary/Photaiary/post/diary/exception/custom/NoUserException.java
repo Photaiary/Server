6655701dbdb919f627eb7e580a021b2b0e698311
LimJuHyeok <ovg07047@naver.com>
2023-02-22 18:07:22 +0900

    Feat(diary/exception): Add NoUserException

--- src/main/java/com/photaiary/Photaiary/post/diary/exception/custom/NoUserException.java
+++ src/main/java/com/photaiary/Photaiary/post/diary/exception/custom/NoUserException.java
@@ -1,2 +1,11 @@
-package com.photaiary.Photaiary.post.diary.exception.custom;public class NoUserException {
+package com.photaiary.Photaiary.post.diary.exception.custom;
+
+import com.photaiary.Photaiary.post.diary.exception.PostCustomException;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class NoUserException extends PostCustomException {
+    public NoUserException(String message) {
+        super(message);
+    }
 }
