1203af0b1e70b78db5a3b92195cf1971966a2f32
LimJuHyeok <ovg07047@naver.com>
2023-02-22 18:06:07 +0900

    Feat: Add DiaryUpdateRequestDto

--- src/main/java/com/photaiary/Photaiary/post/diary/dto/DiaryUpdateRequestDto.java
+++ src/main/java/com/photaiary/Photaiary/post/diary/dto/DiaryUpdateRequestDto.java
@@ -1,2 +1,18 @@
-package com.photaiary.Photaiary.post.diary.dto;public class DiaryUpdateRequestDto {
+package com.photaiary.Photaiary.post.diary.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class DiaryUpdateRequestDto {
+    private String diaryTitle;
+    private String diaryContent;
+
+    @Builder
+    public DiaryUpdateRequestDto(String diaryTitle, String diaryContent) {
+        this.diaryTitle = diaryTitle;
+        this.diaryContent = diaryContent;
+    }
 }
