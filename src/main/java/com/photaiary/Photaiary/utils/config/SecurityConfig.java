88e7370d6327ebed7d8755753ce3e76f50e20ad6
ParkJoungWoo <jug78@naver.com>
2023-02-24 00:00:44 +0900

    test(global/config) : Test For Permission

--- src/main/java/com/photaiary/Photaiary/utils/config/SecurityConfig.java
+++ src/main/java/com/photaiary/Photaiary/utils/config/SecurityConfig.java
@@ -83,7 +83,8 @@ public class SecurityConfig {
                         "/swagger-ui.html",
                         "/webjars/**" ,
                         /*Probably not needed*/ "/swagger.json").permitAll()
-                .antMatchers("/register", "/login","/h2-console/**","/duplicationCheck","/emailCheck","/refresh", "/daily/**").permitAll()
+//                .antMatchers("/register", "/login","/h2-console/**","/duplicationCheck","/emailCheck","/refresh", "/daily/**").permitAll()
+                .antMatchers("/register", "/login","/h2-console/**","/duplicationCheck","/emailCheck","/refresh", "/daily/**","/user/**", "/photo/**","/update/**", "/report/**", "/diary/**","/friend/**").permitAll()
                 // /admin으로 시작하는 요청은 ADMIN 권한이 있는 유저에게만 허용
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 // /user 로 시작하는 요청은 USER 권한이 있는 유저에게만 허용
