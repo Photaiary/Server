80d4871b0173709c8d222ffac5150aa8b32d823d
ParkJoungWoo <jug78@naver.com>
2023-02-23 13:58:35 +0900

    feat(report/dto) : add Report DTO

--- src/main/java/com/photaiary/Photaiary/report/dto/ReportMostDto.java
+++ src/main/java/com/photaiary/Photaiary/report/dto/ReportMostDto.java
@@ -1,2 +1,15 @@
-package com.photaiary.Photaiary.report.dto;public class ReportMostDto {
+package com.photaiary.Photaiary.report.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class ReportMostDto {
+    private String most;
+    private Long photoCount;
+    private Integer month;
+    private String comment;
+    private String top3;
 }
--- src/main/java/com/photaiary/Photaiary/report/dto/ReportResponse.java
+++ src/main/java/com/photaiary/Photaiary/report/dto/ReportResponse.java
@@ -1,2 +1,14 @@
-package com.photaiary.Photaiary.report.dto;public class ReportResponse {
+package com.photaiary.Photaiary.report.dto;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.List;
+@Getter
+@Setter
+public class ReportResponse {
+    private List<Map<String, ReportMostDto>> result;
+    public ReportResponse() {
+        result = new ArrayList<>();
+    }
 }
