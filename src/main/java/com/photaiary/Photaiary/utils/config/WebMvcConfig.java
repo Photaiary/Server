aaa8ec91d275819643aa49fea60e61df626d0a03
sangminee <lsmlee99@gmail.com>
2023-02-23 15:50:46 +0900

    fix: cors

--- src/main/java/com/photaiary/Photaiary/utils/config/WebMvcConfig.java
+++ src/main/java/com/photaiary/Photaiary/utils/config/WebMvcConfig.java
@@ -24,11 +24,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://3.34.242.12:8085") // 허용할 출처
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
-                .allowCredentials(true) // 쿠키 인증 요청 허용
-                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("authorization", "content-type")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000", "http://3.34.242.12:8085") // 허용할 출처
+//                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP method
+//                .allowCredentials(true) // 쿠키 인증 요청 허용
+//                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+//    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
