328d08f4a49c707c75ad08b4a730d1703f5ea0a6
ParkJoungWoo <jug78@naver.com>
2023-02-28 21:24:39 +0900

    fix(config/mvc) : add all URL

--- src/main/java/com/photaiary/Photaiary/utils/config/WebMvcConfig.java
+++ src/main/java/com/photaiary/Photaiary/utils/config/WebMvcConfig.java
@@ -24,7 +24,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://3.34.242.12:8085")
+//                .allowedOrigins("http://localhost:3000", "http://3.34.242.12:8085")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 //                .allowedHeaders("authorization", "content-type")
                 .allowedHeaders("*")
